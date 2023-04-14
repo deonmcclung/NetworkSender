c4ad0ca3d6512094272c9a0d7a7b21ccf8ff0fcd
Deon McClung <deonmcclung@gmail.com>
2023-04-13 20:40:53 -0400

    More tests

---
+++ test/UnitTests/Receiver/ReceiverTests.cpp
@@ -0,0 +1,14 @@
+/**
+ * @brief Unit tests for the Receiver class
+ *
+ * @file ReceiverTests.cpp
+ *
+ * @author Deon McClung
+ * @copyright 2023, Deon McClung, All rights reserved. See LICENSE in the repository root.
+ */
+
+#pragma once
+
+// Class under test
+#include "Receiver/Receiver.cpp"
+
